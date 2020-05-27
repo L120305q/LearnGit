3343d46b0e1a6f4e6d6dfc19b4287631b7583bf2
L120305q <120305486@qq.com>
2020-05-27 09:38:39 +0800

    First Commit

---
+++ LearnGit/LearnGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnGit
+//
+//  Created by Single_Row on 2020/5/27.
+//  Copyright © 2020 Single_Row. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
